de9a9e699aee8f98a97e84297df97d51a7361001
Rishub Kumar <rishub@gmail.com>
2020-04-22 18:53:25 -0700

    add rinkeby network

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,12 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    rinkeby: {
+      host: "localhost",
+      port: "8545",
+      network_id: 4,
+      gas: 4700000,
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
